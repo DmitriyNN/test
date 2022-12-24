050c447ec0443a90e07c9ccb5c04178e5c946dcd
Dimannn <diman1992nn@gmail.com>
2022-12-24 16:29:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 38790;
+        int rub = 20;
+        int bonus = ticketPrice / rub;
+        System.out.println("Сумма бонусов к начислению:" + bonus);
+    }
+}
